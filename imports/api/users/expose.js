0eed491337e8d8a947d3b76dd5860227bbf44426
Theodor Diaconu <diaconu.theodor@gmail.com>
2016-09-24 08:20:27 +0300

    fix field restriction

--- imports/api/users/expose.js
+++ imports/api/users/expose.js
@@ -1,6 +1,6 @@
 import Users from './collection.js';
-import { restrictFields } from 'meteor/cultofcoders:grapher';
+import { Exposure } from 'meteor/cultofcoders:grapher';
 
 Users.expose((filters, options, userId) => {
-    restrictFields(options, ['services', 'createdAt']);
+    Exposure.restrictFields(options, ['services', 'createdAt']);
 });

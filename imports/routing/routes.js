6ee20b7292e1cd1a0080c1fbcd52df6ed018b42e
Theodor Diaconu <diaconu.theodor@gmail.com>
2016-10-05 11:53:45 +0300

    updates

--- imports/routing/routes.js
+++ imports/routing/routes.js
@@ -1,10 +1,11 @@
 import route from './lib/createRoute.js';
 import Home from '/imports/ui/Home';
-import { GrapherLive } from 'meteor/cultofcoders:grapher-live';
+import { GrapherLive, GrapherDocumentation } from 'meteor/cultofcoders:grapher-live';
 import Demo from '/imports/ui/demo/Wrapper';
 import PaginateDemo from '/imports/ui/paginate-demo/Wrapper';
 
 route('/', Home);
 route('/grapher', GrapherLive);
+route('/grapher-docs', GrapherDocumentation);
 route('/demo', Demo);
 route('/demo-paginate/:page?', PaginateDemo);

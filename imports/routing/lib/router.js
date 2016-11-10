1538da2fa7ad184ecd69abec1fd77e81b63d47ac
Theodor Diaconu <diaconu.theodor@gmail.com>
2016-11-10 09:16:20 +0200

    updated to meteor 1.4.1.2 + router

---
+++ imports/routing/lib/router.js
@@ -0,0 +1,5 @@
+import { createRouter } from 'meteor/cultofcoders:meteor-react-routing';
+import App from '/imports/ui/App.jsx';
+
+export default createRouter(App);
+
--- imports/routing/routes.js
+++ imports/routing/routes.js
@@ -1,5 +1,5 @@
 // Helper function to make loading Routes with FlowRouter and React easier
-import route from './lib/createRoute.js';
+import route from './lib/router.js';
 
 // Grapher
 import { GrapherLive, GrapherDocumentation } from 'meteor/cultofcoders:grapher-live';
@@ -14,3 +14,6 @@ import PaginateDemo from '/imports/ui/paginate-demo/Wrapper';
 route('/', Home);
 route('/demo', Demo);
 route('/demo-paginate/:page?', PaginateDemo);
+
+
+console.log(route);
